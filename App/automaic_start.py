076b7b59235a320286ea7be1196348f4f5d6a8ad
Alex Goldman <chergold156@gmail.com>
2024-05-28 17:02:20 +0300

    no message

--- App/automaic_start.py
+++ App/automaic_start.py
@@ -2,13 +2,5 @@ import subprocess
 
 def automaic_start(name, path):
     # Команда, для добавления в регистр автоматического запуска программмы
-    command = fr'reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v {name} /t REG_SZ /d "{path}" /f'
-
-    # Запуск команды
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-
-    # Вывод результата
-    print(stdout.decode())
-    if stderr:
-        print("Error:", stderr.decode())
+    command = f'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v {name} /t REG_SZ /d "{path}" /f'
+    subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
